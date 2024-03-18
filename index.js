7269e6c9d643efc2a37dd2d3e389b5d64dba0177
ziyang-666 <song.ziya@northeastern.edu>
2024-03-17 18:58:31 -0700

    update with google oauth

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi : 'there'});
-})
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile', profile);
+    }
+));
 
-app.listen(5000, () => {
-    console.log('服务器已经启动')
-})
+app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
