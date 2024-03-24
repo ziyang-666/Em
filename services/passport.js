7efc4abf92d62230dc7446b4e8cbf05ec600eb9e
ziyang-666 <song.ziya@northeastern.edu>
2024-03-24 16:48:48 -0700

    update changes

---
+++ services/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys')
+const mongoose = require('mongoose')
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+            // we already have a record wit the given profile ID
+            done(null, existingUser)
+        }
+        // we don't have a user record with this ID, make a new record
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
+    }
+));
