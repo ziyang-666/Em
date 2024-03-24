90b0564a7579f9dd92a4cb121592db6156e5a8c0
ziyang-666 <song.ziya@northeastern.edu>
2024-03-23 18:45:13 -0700

    update mail function

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session');
 const passport = require('passport')
-const keys = require('./config/keys') 
+const keys = require('./config/keys')
+const bodyParser = require('body-parser');
 require('./models/User')
+require('./models/Survey')
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +23,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
